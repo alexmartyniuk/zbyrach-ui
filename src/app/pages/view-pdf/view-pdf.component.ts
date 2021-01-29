e8a331b7f541e9fa891fa04d422974710487fde9
Aleksander Martyniuk <aleksander.martyniuk@scania.com>
2021-01-29 17:05:13 +0100

    Removed spinner for PDF component

--- src/app/pages/view-pdf/view-pdf.component.ts
+++ src/app/pages/view-pdf/view-pdf.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { PDFDocumentProxy } from 'ng2-pdf-viewer';
 import { ActivatedRoute } from '@angular/router';
 import { ApiService } from 'src/app/services/api.service';
-import { UiService } from 'src/app/services/ui.service';
 import { NotificationService } from 'src/app/services/notification.service';
 import { ArticleService } from 'src/app/services/artilcle.service';
 import { TranslateService } from '@ngx-translate/core';
@@ -25,7 +24,6 @@ export class ViewPdfComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private apiService: ApiService,
-    private uiService: UiService,
     private notificationService: NotificationService,
     private articleService: ArticleService,
     private translate: TranslateService) { }
@@ -33,19 +31,16 @@ export class ViewPdfComponent implements OnInit {
   ngOnInit() {
     this.articleId = this.route.snapshot.paramMap.get('articleId');
     this.userId = this.route.snapshot.paramMap.get('userId');
-    this.uiService.showSpinner();
     this.pdfSrc = this.apiService.getArticleInlinePdfUrl(this.articleId, this.userId);
   }
 
   onLoadComplete(pdf: PDFDocumentProxy) {
     this.pdfLoaded = true;
-    this.uiService.hideSpinner();
     this.pagesCount = pdf.numPages;
   }
 
   onError(error: any) {
     this.isError = true;
-    this.uiService.hideSpinner();
     this.notificationService.showErrorMessage(this.translate.instant('General.ServerError'));
   }
 
